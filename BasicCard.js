44682b518e545d291cd1a7ea875b434990ad5f66
Miles Helm <mileshelm@Miless-MacBook-Pro.local>
2017-08-09 22:24:28 -0400

    Complete

--- BasicCard.js
+++ BasicCard.js
@@ -8,3 +8,17 @@ function BasicCard(front, back) {
 
 var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington");
 
+var poseQuestion = function() {
+	inquirer.prompt({
+		name: "user_input",
+		message: firstPresident.front
+	}).then(function(answers){
+		if (answers.user_input === firstPresident.back) {
+			console.log("Right you are!");
+		} else {
+			console.log("Wrong. The correct answer is " + firstPresident.back);
+		}
+	})
+}
+
+poseQuestion();
